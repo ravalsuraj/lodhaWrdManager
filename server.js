9cb64585f75a577338b5f22cbc5af9afd82a7ba2
Suraj Raval <raval.suraj@gmail.com>
2020-03-06 11:10:37 +0530

    Added a dummy log

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const apiRouter = require("./routers/apiRouter");
 const webRouter = require("./routers/webRouter");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const moment = require('moment-timezone');
+const moment = require("moment-timezone");
 
 const logger = require("./logger");
-const config= require("./config")
+const config = require("./config");
 const PORT = config.port || 4000;
 
 app.set("view engine", "pug");
@@ -21,20 +21,17 @@ morgan.token("date", (req, res, tz) => {
     .tz(tz)
     .format();
 });
-morgan.format(
-  "myformat",
-  config.logs.morgan.format
-);
+morgan.format("myformat", config.logs.morgan.format);
 app.use(morgan("myformat", { stream: { write: message => logger.info(message.trim()) } }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+console.log("adding a log");
 app.use("/", webRouter);
 app.use("/api", apiRouter);
 
-app.listen(PORT, () =>{
-  logger.log('info',  "Lodha Web App listening on port " + PORT);
+app.listen(PORT, () => {
+  logger.log("info", "Lodha Web App listening on port " + PORT);
 });
 // npm config set proxy 165.225.104.34:80
 // npm config set https - proxy 165.225.104.34:80
